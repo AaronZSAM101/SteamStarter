f6825f42350de059715e323e259aaea9b701d310
Aaron Liu <CaptainAaron101@163.com>
2023-04-11 22:40:55 +0800

    保护个人隐私删除账号用户名

--- Login to Steam.bat
+++ Login to Steam.bat
@@ -7,7 +7,7 @@ echo Please entry the login area(1.China; 2.Turkey):
 rem Determine the login area
 set /p area=
 rem Modify the registry
-if /i %area% == 1 (REG ADD HKEY_CURRENT_USER\SOFTWARE\Valve\Steam /v AutoLoginUser /t REG_SZ /d aaron_zsam /f)
-if /i %area% == 2 (REG ADD HKEY_CURRENT_USER\SOFTWARE\Valve\Steam /v AutoLoginUser /t REG_SZ /d xaq26858 /f)
+if /i %area% == 1 (REG ADD HKEY_CURRENT_USER\SOFTWARE\Valve\Steam /v AutoLoginUser /t REG_SZ /d YourUserName /f)
+if /i %area% == 2 (REG ADD HKEY_CURRENT_USER\SOFTWARE\Valve\Steam /v AutoLoginUser /t REG_SZ /d YourUserName /f)
 rem Start Steam
-start D:\Steam\steam.exe
+start "Your 'Steam.exe' Path"
